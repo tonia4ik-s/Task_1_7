2cef1b12ea6b939ff7d302a305f052282450a2d2
Task_1_1 <cooltone4ka@gmail.com>
2018-11-01 14:11:26 +0200

    /

---
+++ src/main/java/com/tonia/Main.java
@@ -0,0 +1,12 @@
+package com.tonia;
+
+public class Main {
+    public static void main(String[] args) {
+        int k = 1;
+        for(int i=2; k <= 20; i=i*2) {
+            k++;
+            System.out.println(i);
+        }
+
+    }
+}
